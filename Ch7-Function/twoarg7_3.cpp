4cb7199ce8cb6938966ef699410ba865ced1ca56
w52191 <wangting52918@gmail.com>
2016-12-31 23:02:58 -0500

    Ting commit

--- Ch7-Function/twoarg7_3.cpp
+++ Ch7-Function/twoarg7_3.cpp
@@ -1,17 +1,31 @@
 #include<iostream>
 #include<string>
 
-int main(){
-  using namespace std;
+using namespace std;
+void n_char(char, int);
 
+int main(){
   char ch;
-  string str;
-  char arr[10] = "string";
-  cout << arr << endl;
-  cout << "Please enter a character:" << endl;
-  cin >> arr;
-  cout << endl;
-  cout << arr << endl;
-  cout << arr[4];
+  int times;
+  //string str;
+  //char arr[10] = "string";
+  //cout << arr << endl;
+  cout << "Please enter a number:" << endl;
+  cin >> times;
+  while(ch != 'q'){
+    cout << "Please enter a character:" << endl;
+    cin >> ch;
+    cout << endl;
+    n_char(ch, times);
+    //cout << arr[4];
+  }
   return 0;
+}
+
+void n_char(char ch, int n){
+  while(n--){ //n vs 0 first, then n-1, then goes into the loop
+      cout << n << " "; //test n
+      cout << ch;
+    }
+  cout << "value of n:" << n << endl; //test
 }
