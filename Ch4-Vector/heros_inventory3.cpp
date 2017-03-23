defa5fa266189c90b910b275bd90fc48d0e1036a
Ting Wang <Ting Wang>
2017-03-23 15:53:44 -0400

    Ting

---
+++ Ch4-Vector/heros_inventory3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+  vector<string> inventory;
+  inventory.push_back("sword");
+  inventory.push_back("armor");
+  inventory.push_back("shield");
+
+  vector<string>::iterator myIterator;
+  vector<string>::const_iterator iter;
+
+  cout << "Your items:\n";
+  for(iter = inventory.begin(); iter != inventory.end(); iter++){
+    cout << *iter << endl;
+  }
+  cout << "\nTrade your sword for a battle axe.";
+  myIterator = inventory.begin();
+  *myIterator = "battle axe";
+  cout << "\nYour items:\n";
+  for(myIterator = inventory.begin(); myIterator != inventory.end(); myIterator++){
+    cout << *myIterator << endl;
+  }
+  myIterator = inventory.begin();
+  cout << "\nThe item name " << *myIterator << " has ";
+  cout << (*myIterator).size() << " letters in it.\n";
+
+  cout << "\nThe item name " << *myIterator << " has ";
+  cout << myIterator->size() << " letters in it.\n";
+
+  cout << "\nYou recover a crossbow from slain enemy.";
+  inventory.insert(inventory.begin(),"crossbow");
+  cout << "\nYour items:\n";
+  for(iter = inventory.begin(); iter != inventory.end(); iter++){
+    cout << *iter << endl;
+  }
+
+  cout << "\nYour armor is destroyed in a fierce battle.";
+  inventory.erase(inventory.begin()+2);
+  cout << "\nYour items:\n";
+  for(iter = inventory.begin(); iter != inventory.end(); iter++){
+    cout << *iter << endl;
+  }
+  return 0;
+}
